4145c8858f8c6f284c3bbf2b97d6b9fa7cdbcd08
Niveen <niveenabutamam@gmail.com>
2025-08-27 13:33:13 +0300

    add fun

--- project.py
+++ project.py
@@ -5,4 +5,15 @@ def Add_reservation():
     time = input(" time: ")
     appointments.append((name, date, time))
     print("Your reservation has been completed successfully\n")
+def show_appointments():
+    
+    if not appointments:
+        print("No reservation unfortunately\n")  
+        return
+    
+    print("Appointments List:") 
+    for i, app in enumerate(appointments, start=1):
+        name, date, time = app 
+        print(f"{i}) Name: {name} | Date: {date} | Time: {time}")
+    print()  
 
